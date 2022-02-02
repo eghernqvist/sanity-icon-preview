deb30f6c14ac1e0e617b84e3c3d21515cf273d02
herge <emil.hernqvist@pinchonation.se>
2022-02-02 08:55:34 +0100

    Implementation and readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import * as icons from "@sanity/icons";
+import camelcase from "camelcase";
+import { useState } from "react";
+import "./App.css";
+
+console.log("icons", icons);
+
+const regscape = (s) => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"); // eslint-disable-line
+/**
+ * @param {string} query - query to look for
+ * @param {Object} collection - the object to search in
+ * @param {...any} keys - the keys to look at in the object
+ */
+const regex = (query, value) => new RegExp(regscape(query), "gi").test(value);
 
 function App() {
+  const [query, setQuery] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "row wrap",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div style={{ width: "100%", padding: '1rem' }}>
+          <input
+            style={{ width: "100%", height: '48px', padding: '0 1rem', borderRadius: 4, boxSizing: 'border-box' }}
+            placeholder="Search for icon"
+            onChange={({ target: { value } }) => {
+              setQuery(value);
+            }}
+          />
+        </div>
+        {Object.keys(icons.icons)
+          .filter((iconName) => {
+            if (!query || query.length === 0) {
+              return true;
+            }
+            return regex(query, camelcase(iconName));
+          })
+          .map((key) => ({ name: key, Component: icons.icons[key] }))
+          .map(({ name, Component }) => {
+            return (
+              <div style={{ padding: "1rem" }} key={name}>
+                <h5 style={{ margin: 0, textTransform: 'capitalize' }}>{camelcase(name)}</h5>
+                <Component style={{ fontSize: 24 }} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
